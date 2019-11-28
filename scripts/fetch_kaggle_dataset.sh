ac7ae0eb34fd34383ab174005867123d729c3be9
eltina <eltinawh@yahoo.com>
2019-11-28 08:31:53 +0700

    Fixing typo in circle ci

--- scripts/fetch_kaggle_dataset.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kaggle competitions download -c house-prices-advanced-regression-techniques -f train.csv -p packages/regression_model/regression_model/datasets/
-kaggle competitions download -c house-prices-advanced-regression-techniques -f test.csv -p packages/regression_model/regression_model/datasets/
