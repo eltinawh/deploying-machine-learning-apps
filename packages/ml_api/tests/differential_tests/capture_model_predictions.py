145580ebc5c72256d0efe9608211b122c6872605
eltina <eltinawh@yahoo.com>
2020-02-03 09:35:23 +0700

    change directory of capture_model_predictions.py

--- packages/ml_api/tests/differential_tests/capture_model_predictions.py
+++ packages/ml_api/tests/differential_tests/capture_model_predictions.py
@@ -12,10 +12,11 @@ from regression_model.processing.data_management import load_dataset
 from api import config
 
 
-def capture_predictions() -> None:
+def capture_predictions(
+        *,
+        save_file: str = 'test_data_predictions.csv'):
     """Save the test data predictions to a CSV."""
 
-    save_file = 'test_data_predictions.csv'
     test_data = load_dataset(file_name='test.csv')
 
     # we take a slice with no input validation issues
@@ -28,7 +29,9 @@ def capture_predictions() -> None:
 
     # hack here to save the file to the regression model
     # package of the repo, not the installed package
-    predictions_df.to_csv(f'{config.PACKAGE_ROOT}/{save_file}')
+    predictions_df.to_csv(
+        f'{config.PACKAGE_ROOT.parent}/'
+        f'regression_model/regression_model/datasets/{save_file}')
 
 
 if __name__ == '__main__':
