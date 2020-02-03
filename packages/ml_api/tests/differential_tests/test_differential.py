031eb5cdedc560d98964af081643d071b31eea6e
eltina <eltinawh@yahoo.com>
2020-02-03 10:43:17 +0700

    please work

--- packages/ml_api/tests/differential_tests/test_differential.py
+++ packages/ml_api/tests/differential_tests/test_differential.py
@@ -9,6 +9,7 @@ import pytest
 
 from api import config
 
+
 @pytest.mark.skip
 @pytest.mark.differential
 def test_model_prediction_differential(
@@ -23,12 +24,12 @@ def test_model_prediction_differential(
     # Load the saved previous model predictions
     previous_model_df = pd.read_csv(f'{config.PACKAGE_ROOT}/{save_file}')
     previous_model_predictions = previous_model_df.predictions.values
-    
-    test_data = load_dataset(file_name=model_config.TESTING_DATA_FILE)
-    multiple_test_json = test_data[99:600]
+
+    test_data = load_dataset(file_name=f'{save_file}')
+    multiple_test_input = test_data[99:600]
 
     # When
-    current_result = make_prediction(input_data=multiple_test_json)
+    current_result = make_prediction(input_data=multiple_test_input)
     current_model_predictions = current_result.get('predictions')
 
     # Then
@@ -39,15 +40,15 @@ def test_model_prediction_differential(
     # Perform the differential test
     for previous_value, current_value in zip(
             previous_model_predictions, current_model_predictions):
-        
+
         # convert numpy float64 to Python float.
         previous_value = previous_value.item()
         current_value = current_value.item()
 
-        # rel_tol is the relative tolerance - it is the maximum allowed
+        # rel_tol is the relative tolerance – it is the maximum allowed
         # difference between a and b, relative to the larger absolute
         # value of a or b. For example, to set a tolerance of 5%, pass
         # rel_tol=0.05.
-        assert math.isclose(previous_value, 
-                            current_value, 
+        assert math.isclose(previous_value,
+                            current_value,
                             rel_tol=model_config.ACCEPTABLE_MODEL_DIFFERENCE)
